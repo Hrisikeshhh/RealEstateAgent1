e1419c04721683074987777d8c9d7b9e79b8a54e
Hrisikeshhh <hrisikeshgogoi09@gmail.com>
2026-03-07 18:37:16 +0530

    add

--- script.js
+++ script.js
@@ -38,11 +38,23 @@ window.onclick = function(event) {
 }
 })
 
+// navigations btns
 
 document.querySelector('.visit-btn').addEventListener('click',()=>{
   window.location.href = "contact.html";
 })
 
+document.querySelector('.contactForm').addEventListener('click',()=>{
+  document.querySelector('.form').scrollIntoView({
+    behavior:"smooth"
+  })
+})
+
+document.querySelector('#navBtn').addEventListener('click',()=>{
+   document.querySelector('.form').scrollIntoView({
+    behavior:"smooth"
+   })
+})
 
 fetch('data.json')
 .then((response)=>{
@@ -71,6 +83,76 @@ fetch('data.json')
 
          })
   }) 
+
+  // counter
+  //  clients Counter
+
+ function animateCount(element, target) {
+    let count = 0;
+    let step = Math.ceil(target / 180); // speed of increase
+    let interval = setInterval(() => {
+      count += step;
+      if (count >= target) {
+        count = target; // stop at target
+        clearInterval(interval);
+        element.textContent = count + '+'
+      }
+      else{
+      element.textContent = count ;
+      }
+    }, 20); // update every 20ms
+  }
+
+
+
+  // Use IntersectionObserver so it starts when div is visible
+  const clientDiv = document.getElementById("HouseShown");
+  const observer = new IntersectionObserver(entries => {
+    if (entries[0].isIntersecting) {
+      animateCount(clientDiv, 120); // suppose 1500 clients
+      observer.disconnect(); // run only once
+    }
+  });
+
+  observer.observe(clientDiv);
+
+
+
+  // Use IntersectionObserver so it starts when div is visible
+  const clientDiv1 = document.getElementById("clientServed");
+  const observer1 = new IntersectionObserver(entries => {
+    if (entries[0].isIntersecting) {
+      animateCount(clientDiv1, 35); // suppose 1500 clients
+      observer1.disconnect(); // run only once
+    }
+  });
+
+  observer1.observe(clientDiv1);
+
+
+
+  // Use IntersectionObserver so it starts when div is visible
+  const clientDiv2 = document.getElementById("propertiesListed");
+  const observer2 = new IntersectionObserver(entries => {
+    if (entries[0].isIntersecting) {
+      animateCount(clientDiv2, 50); // suppose 1500 clients
+      observer2.disconnect(); // run only once
+    }
+  });
+
+  observer2.observe(clientDiv2);
+
+// Use IntersectionObserver so it starts when div is visible
+  const clientDiv3 = document.getElementById("rating");
+  const observer3 = new IntersectionObserver(entries => {
+    if (entries[0].isIntersecting) {
+      animateCount(clientDiv3, 4.5); // suppose 1500 clients
+      observer3.disconnect(); // run only once
+    }
+  });
+
+  observer3.observe(clientDiv3);
+
 
   let desktop = gsap.matchMedia();
 
